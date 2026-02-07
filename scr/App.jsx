3c0dc667d0b416910b654caa4db950efbebf4812
Leelanath <122965242+leelanathT@users.noreply.github.com>
2026-02-07 16:21:18 +0530

    Create App.jsx

---
+++ scr/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header'
+import About from './components/About'
+import Skills from './components/Skills'
+import Architecture from './components/Architecture'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Architecture />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
